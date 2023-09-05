f78f6102384da85aa9faf59818e3af86049a605e
Manish Kumawat <111861277+manish70945656555@users.noreply.github.com>
2023-09-05 12:03:17 +0530

    Update train_pipeline.py

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -50,6 +50,7 @@ class TraininingPipeline:
 
     def run_pipeline(self):
         try:
+            #running training pipeline
             feature_store_file_path = self.start_data_ingestion()
             train_arr, test_arr,preprocessor_path = self.start_data_transformation(feature_store_file_path)
             r2_square = self.start_model_training(train_arr, test_arr)
