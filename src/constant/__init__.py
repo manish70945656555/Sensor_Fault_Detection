5b143ae712bf0660cf36ae6201aee50dcaee1ac2
Manish Kumawat <111861277+manish70945656555@users.noreply.github.com>
2023-09-06 06:17:50 +0530

    Update __init__.py

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -6,7 +6,7 @@ MONGO_DATABASE_NAME = "WaferFault"
 MONGO_COLLECTION_NAME = "WaferFaultDetection"
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL = "mongodb+srv://manishkumawat0803:manishkumawat08034@cluster0.necirxl.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL = "mongodb+srv://manishkumawat0803:sansad70@cluster0.necirxl.mongodb.net/?retryWrites=true&w=majority"
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
